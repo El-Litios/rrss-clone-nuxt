fdd14129f6d86c82fbb79d1b7fe59a422163a957
Carlos Guzmán Rojas <63490179+El-Litios@users.noreply.github.com>
2024-01-17 09:15:05 -0300

    added who to follow iterator with data

--- components/SideBar/Right/index.vue
+++ components/SideBar/Right/index.vue
@@ -16,15 +16,32 @@
 
     <!-- Preview Card "Who to follow"-->
     <SideBarRightPreviewCard title="Who to follow">
-      <SideBarRightPreviewCardItem v-for="items in whoToFollowItems">
-        <div class="flex flex-row justify-between p-2">
+      <SideBarRightPreviewCardItem v-for="whoToFollow in whoToFollowItems">
+        <div class="flex flex-row items-center justify-between p-2">
           <div class="flex flex-row">
-            <img class="w-10 h-10 rounded-full" src="" alt="aaa">
+            <img
+              class="w-10 h-10 rounded-full"
+              :src="whoToFollow.avatar"
+              :alt="whoToFollow.name"
+            />
+
+            <div class="flex flex-col ml-2">
+              <h1 class="text-sm font-bold text-gray-900 dark:text-white">
+                {{ whoToFollow.name }}
+              </h1>
+              <p class="text-xs text-gray-400">{{ whoToFollow.handle }}</p>
+            </div>
+          </div>
+          <div class="flex h-full">
+            <button
+              class="px-4 py-2 text-xs font-bold text-white bg-black rounded-full dark:text-black dark:bg-white"
+            >
+              Follow
+            </button>
           </div>
         </div>
       </SideBarRightPreviewCardItem>
     </SideBarRightPreviewCard>
-
   </div>
 </template>
 
@@ -47,7 +64,8 @@ const whatHappeningItems = ref([
 const whoToFollowItems = ref([
   {
     name: "John Doe",
-    count: "19.9k tweets",
+    handle: "@johndoe",
+    avatar: "https://picsum.photos/200/200",
   },
 ]);
 </script>
